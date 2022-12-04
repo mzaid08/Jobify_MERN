51ba70918515f7f91fea39b45f438797d41ac2d8
mzaid08 <imzaidmohd@gmailcom>
2022-12-04 14:47:26 +0530

    adding logo

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,7 +2,29 @@ import logo from "../assets/images/logo.svg";
 import main from "../assets/images/main.svg";
 
 const Landing = () => {
-  return <h4>Landing Page</h4>;
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt="jobify" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            Job <span>traking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi fugit,
+            maxime est dolores itaque dolorum reprehenderit voluptate ipsum,
+            neque ducimus quae ratione reiciendis. Consequatur illum in dolor
+            voluptate. Perferendis, eligendi? Distinctio recusandae minima
+            necessitatibus neque mollitia architecto eveniet molestias maiores?
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </main>
+  );
 };
 
 export default Landing;
